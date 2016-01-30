b6e67a36eca7c4c25d846b2d7d218ef324bdadc7
aleix26 <aleix66@hotmail.com>
2016-01-30 12:06:52 +0100

    mysqlc#

--- PractMysql/PractMysql/Form1.Designer.cs
+++ PractMysql/PractMysql/Form1.Designer.cs
@@ -28,35 +28,174 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this._c_DataSet = new PractMysql._c_DataSet();
+            this.cDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.clientsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.clientsTableAdapter = new PractMysql._c_DataSetTableAdapters.clientsTableAdapter();
+            this.idClientDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nomDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cognom1DataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.adreçaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.codiPostalDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.poblacioDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.provinciaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.telefonDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.faxDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.emailDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this._c_DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clientsBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
-            // checkBox1
+            // dataGridView1
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(67, 67);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(80, 17);
-            this.checkBox1.TabIndex = 0;
-            this.checkBox1.Text = "checkBox1";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idClientDataGridViewTextBoxColumn,
+            this.nomDataGridViewTextBoxColumn,
+            this.cognom1DataGridViewTextBoxColumn,
+            this.adreçaDataGridViewTextBoxColumn,
+            this.codiPostalDataGridViewTextBoxColumn,
+            this.poblacioDataGridViewTextBoxColumn,
+            this.provinciaDataGridViewTextBoxColumn,
+            this.telefonDataGridViewTextBoxColumn,
+            this.faxDataGridViewTextBoxColumn,
+            this.emailDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.clientsBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(277, 55);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(438, 190);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // _c_DataSet
+            // 
+            this._c_DataSet.DataSetName = "_c_DataSet";
+            this._c_DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // cDataSetBindingSource
+            // 
+            this.cDataSetBindingSource.DataSource = this._c_DataSet;
+            this.cDataSetBindingSource.Position = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(308, 278);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // clientsBindingSource
+            // 
+            this.clientsBindingSource.DataMember = "clients";
+            this.clientsBindingSource.DataSource = this._c_DataSet;
+            // 
+            // clientsTableAdapter
+            // 
+            this.clientsTableAdapter.ClearBeforeFill = true;
+            // 
+            // idClientDataGridViewTextBoxColumn
+            // 
+            this.idClientDataGridViewTextBoxColumn.DataPropertyName = "id_Client";
+            this.idClientDataGridViewTextBoxColumn.HeaderText = "id_Client";
+            this.idClientDataGridViewTextBoxColumn.Name = "idClientDataGridViewTextBoxColumn";
+            // 
+            // nomDataGridViewTextBoxColumn
+            // 
+            this.nomDataGridViewTextBoxColumn.DataPropertyName = "Nom";
+            this.nomDataGridViewTextBoxColumn.HeaderText = "Nom";
+            this.nomDataGridViewTextBoxColumn.Name = "nomDataGridViewTextBoxColumn";
+            // 
+            // cognom1DataGridViewTextBoxColumn
+            // 
+            this.cognom1DataGridViewTextBoxColumn.DataPropertyName = "Cognom1";
+            this.cognom1DataGridViewTextBoxColumn.HeaderText = "Cognom1";
+            this.cognom1DataGridViewTextBoxColumn.Name = "cognom1DataGridViewTextBoxColumn";
+            // 
+            // adreçaDataGridViewTextBoxColumn
+            // 
+            this.adreçaDataGridViewTextBoxColumn.DataPropertyName = "Adreça";
+            this.adreçaDataGridViewTextBoxColumn.HeaderText = "Adreça";
+            this.adreçaDataGridViewTextBoxColumn.Name = "adreçaDataGridViewTextBoxColumn";
+            // 
+            // codiPostalDataGridViewTextBoxColumn
+            // 
+            this.codiPostalDataGridViewTextBoxColumn.DataPropertyName = "CodiPostal";
+            this.codiPostalDataGridViewTextBoxColumn.HeaderText = "CodiPostal";
+            this.codiPostalDataGridViewTextBoxColumn.Name = "codiPostalDataGridViewTextBoxColumn";
+            // 
+            // poblacioDataGridViewTextBoxColumn
+            // 
+            this.poblacioDataGridViewTextBoxColumn.DataPropertyName = "Poblacio";
+            this.poblacioDataGridViewTextBoxColumn.HeaderText = "Poblacio";
+            this.poblacioDataGridViewTextBoxColumn.Name = "poblacioDataGridViewTextBoxColumn";
+            // 
+            // provinciaDataGridViewTextBoxColumn
+            // 
+            this.provinciaDataGridViewTextBoxColumn.DataPropertyName = "Provincia";
+            this.provinciaDataGridViewTextBoxColumn.HeaderText = "Provincia";
+            this.provinciaDataGridViewTextBoxColumn.Name = "provinciaDataGridViewTextBoxColumn";
+            // 
+            // telefonDataGridViewTextBoxColumn
+            // 
+            this.telefonDataGridViewTextBoxColumn.DataPropertyName = "Telefon";
+            this.telefonDataGridViewTextBoxColumn.HeaderText = "Telefon";
+            this.telefonDataGridViewTextBoxColumn.Name = "telefonDataGridViewTextBoxColumn";
+            // 
+            // faxDataGridViewTextBoxColumn
+            // 
+            this.faxDataGridViewTextBoxColumn.DataPropertyName = "Fax";
+            this.faxDataGridViewTextBoxColumn.HeaderText = "Fax";
+            this.faxDataGridViewTextBoxColumn.Name = "faxDataGridViewTextBoxColumn";
+            // 
+            // emailDataGridViewTextBoxColumn
+            // 
+            this.emailDataGridViewTextBoxColumn.DataPropertyName = "E-mail";
+            this.emailDataGridViewTextBoxColumn.HeaderText = "E-mail";
+            this.emailDataGridViewTextBoxColumn.Name = "emailDataGridViewTextBoxColumn";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.checkBox1);
+            this.ClientSize = new System.Drawing.Size(738, 380);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this._c_DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clientsBindingSource)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private _c_DataSet _c_DataSet;
+        private System.Windows.Forms.BindingSource cDataSetBindingSource;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.BindingSource clientsBindingSource;
+        private _c_DataSetTableAdapters.clientsTableAdapter clientsTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idClientDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nomDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cognom1DataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn adreçaDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn codiPostalDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn poblacioDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn provinciaDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn telefonDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn faxDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn emailDataGridViewTextBoxColumn;
     }
 }
 
--- PractMysql/PractMysql/Form1.cs
+++ PractMysql/PractMysql/Form1.cs
@@ -16,5 +16,12 @@ namespace PractMysql
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla '_c_DataSet.clients' Puede moverla o quitarla según sea necesario.
+            this.clientsTableAdapter.Fill(this._c_DataSet.clients);
+
+        }
     }
 }
